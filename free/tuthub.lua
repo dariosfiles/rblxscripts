d0e01acf733b8bca7be35d0bd60f4bdb203a1d56
dariosfiles <javierpanchosk@gmail.com>
2026-05-07 21:19:13 +0200

    Create tuthub.lua

---
+++ free/tuthub.lua
@@ -0,0 +1,8 @@
+--[[
+Welcome to TutHub Slotted
+PAID SOON free for limited time, go on use before pay
+
+Ty diddyfun for hosting! <3
+--]]
+-- Loader:
+loadstring(game:HttpGet("https://paste.rs/OG15Q"))()
